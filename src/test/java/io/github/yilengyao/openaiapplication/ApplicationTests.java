981ffd27a302f942a57f006cda1181e91977d52f
yilengyao <yilengyao@gmail.com>
2023-08-28 23:30:23 +0000

    first commit

---
+++ src/test/java/io/github/yilengyao/openaiapplication/ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.yilengyao.openaiapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
